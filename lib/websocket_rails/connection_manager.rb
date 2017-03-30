f819390dbaae035b3c2e75cb34a3b09f910ffa0b
Mukesh <ashu.3333d@gmail.com>
2017-03-30 13:24:48 +0530

    corrected interprocess comunication.

--- lib/websocket_rails/connection_manager.rb
+++ lib/websocket_rails/connection_manager.rb
@@ -79,6 +79,7 @@ module WebsocketRails
       raise InvalidConnectionError unless Connection.websocket?(request.env)
 
       connection = Connection.new(request, dispatcher)
+      @dispatcher.process_inbound
 
       register_user_connection connection
 
